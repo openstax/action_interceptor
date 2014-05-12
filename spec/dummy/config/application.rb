651ae401ba8490ac16e39f584eb9c4b24344913a
Dante Soares <dms3@rice.edu>
2014-05-12 12:35:35 -0500

    Fixed dummy app config

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "fine_print"
+require "action_interceptor"
 
 module Dummy
   class Application < Rails::Application
